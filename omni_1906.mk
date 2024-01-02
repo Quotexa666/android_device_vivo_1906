cfd380d4b1e5a33fa5dd74abc35b66115ea7ddf7
Quotexa666 <155422006+Quotexa666@users.noreply.github.com>
2024-01-02 23:24:29 +0500

    Update omni_1906.mk

--- omni_1906.mk
+++ omni_1906.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from 1906 device
 $(call inherit-product, device/vivo/1906/device.mk)
 
 PRODUCT_DEVICE := 1906
-PRODUCT_NAME := aosp_1906
+PRODUCT_NAME := twrp_1906
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := vivo 1906
 PRODUCT_MANUFACTURER := vivo
